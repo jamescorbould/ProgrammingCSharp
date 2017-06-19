8a1fd146a76b43bf3b94fc425715f41f87c6db49
James Corbould <jamesc@datacom.co.nz>
2017-06-19 21:28:15 +1200

    Added new Order class for experimenting with interfaces.

---
+++ 2_CreateTypes/Interfaces.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CreateTypes
+{
+    class Order : IComparable
+    {
+        public DateTime Created { get; set; }
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            Order o = obj as Order;
+
+            if (o == null)
+            {
+                throw new ArgumentException("Object is not an Order");
+            }
+
+            return this.Created.CompareTo(o.Created);
+        }
+    }
+}
--- 2_CreateTypes/Program.cs
+++ 2_CreateTypes/Program.cs
@@ -10,26 +10,47 @@ namespace _2_CreateTypes
     {
         static void Main(string[] args)
         {
-            Calculator calc = new Calculator();
-            Product p = new Product()
+            //Calculator calc = new Calculator();
+            //Product p = new Product()
+            //{
+            //    Price = 100M
+            //};
+
+            //Console.WriteLine("Product price = {0}", p.Price);
+            //Console.WriteLine("Product discounted price = {0}", calc.CalculateDiscount(p));
+
+            //Derived d = new Derived();
+            //Derived d2 = new Derived2();
+
+            //Console.WriteLine(d.MyMethod());
+            //Console.WriteLine(d2.MyMethod());
+
+            //Money m = new Money(42.42M);
+            //decimal amount = m;  // Implicit conversion.
+            //Console.WriteLine(amount);
+            //int truncatedAmount = (int)m;  // Explicit conversion (casting).
+            //Console.WriteLine(truncatedAmount);
+
+            List<Order> orders = new List<Order>
             {
-                Price = 100M
+                new Order { Created = new DateTime(2016, 6, 19) },
+                new Order { Created = new DateTime(2014, 6, 19) },
+                new Order { Created = new DateTime(2017, 6, 19) },
+                new Order { Created = new DateTime(2011, 6, 19) }
             };
 
-            Console.WriteLine("Product price = {0}", p.Price);
-            Console.WriteLine("Product discounted price = {0}", calc.CalculateDiscount(p));
+            foreach (Order o in orders)
+            {
+                Console.WriteLine(o.Created.ToString());
+            }
 
-            Derived d = new Derived();
-            Derived d2 = new Derived2();
+            orders.Sort();
+            Console.WriteLine();
 
-            Console.WriteLine(d.MyMethod());
-            Console.WriteLine(d2.MyMethod());
-
-            Money m = new Money(42.42M);
-            decimal amount = m;  // Implicit conversion.
-            Console.WriteLine(amount);
-            int truncatedAmount = (int)m;  // Explicit conversion (casting).
-            Console.WriteLine(truncatedAmount);
+            foreach (Order o in orders)
+            {
+                Console.WriteLine(o.Created.ToString());
+            }
 
             Console.ReadKey();
         }
