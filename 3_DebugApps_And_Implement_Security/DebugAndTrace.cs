7f7fabf14636ca26d93bea358da41aa890546951
James Corbould <jamesc@datacom.co.nz>
2017-07-08 22:52:37 +1200

    Added sample code for executing debug and trace info.

--- 3_DebugApps_And_Implement_Security/DebugAndTrace.cs
+++ 3_DebugApps_And_Implement_Security/DebugAndTrace.cs
@@ -1,16 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace _3_DebugApps_And_Implement_Security
 {
     public static class DebugAndTrace
     {
-        public static void testDebug()
+        static void Main(string[] args)
         {
+            //testDebug();
+            //testTrace();
+            //PerfCounters();
+            IncrementCustomCounters();
+            Console.ReadKey();
+        }
+        public static void TestDebug()
+        {
+            // Output will be return to the Output window in Visual Studio.
+            Debug.WriteLine("Starting application");
+            Debug.Indent();
+            int i = 1 + 2;
+            Debug.Assert(i == 3);
+            Debug.WriteLineIf(i > 0, "i is greater than 0");
+        }
 
+        public static void TestTrace()
+        {
+            TraceSource traceSource = new TraceSource("myTraceSource", SourceLevels.All);
+
+            traceSource.TraceInformation("Tracing application...");
+            traceSource.TraceEvent(TraceEventType.Critical, 0, "Critical trace");
+            traceSource.TraceData(TraceEventType.Information, 1, new object[] { "a", "b", "c" });
+
+            traceSource.Flush();
+            traceSource.Close();
+        }
+
+        public static void PerfCounters()
+        {
+            Console.WriteLine("Press escape key to stop");
+
+            using (PerformanceCounter pc = new PerformanceCounter("Memory", "Available Bytes"))
+            {
+                string text = "Available memory: ";
+                Console.Write(text);
+                do
+                {
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.Write(pc.RawValue);
+                        Console.SetCursorPosition(text.Length, Console.CursorTop);
+                    }
+                }
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+        }
+
+        public static void IncrementCustomCounters()
+        {
+            if (CreatePerfCounters())
+            {
+                Console.WriteLine("Created performance counters");
+                Console.WriteLine("Please restart application");
+                Console.ReadKey();
+                return;
+            }
+
+            var totalOperationsCounter = new PerformanceCounter(
+                "MyCategory",
+                "# operations executed",
+                "",
+                false);
+            var operationsPerSecondCounter = new PerformanceCounter(
+                "MyCategory",
+                "# operations / sec",
+                "",
+                false);
+
+            totalOperationsCounter.Increment();
+            operationsPerSecondCounter.Increment();
+        }
+
+        public static bool CreatePerfCounters()
+        {
+            if (!PerformanceCounterCategory.Exists("MyCategory"))
+            {
+                CounterCreationDataCollection counters = new CounterCreationDataCollection
+                {
+                    new CounterCreationData(
+                        "# operations executed",
+                        "Total number of operations executed",
+                        PerformanceCounterType.NumberOfItems32),
+                    new CounterCreationData(
+                        "# operations / sec",
+                        "Number of operations executed per second",
+                        PerformanceCounterType.RateOfCountsPerSecond32)
+                };
+
+                PerformanceCounterCategory.Create("MyCategory", "MyCategoryHelp", PerformanceCounterCategoryType.SingleInstance, counters);
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
