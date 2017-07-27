b76fa9e2af0b321340ef466c8a7da2773443cc53
James Corbould <jamesc@datacom.co.nz>
2017-07-27 17:30:39 +1200

    Added examples of XML doc and XPath.

--- 4_Implement_Data_Access/Program.cs
+++ 4_Implement_Data_Access/Program.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Xml;
+using System.Xml.XPath;
 
 namespace _4_Implement_Data_Access
 {
@@ -21,8 +22,10 @@ namespace _4_Implement_Data_Access
             //Task t = UpdateRows();
             //Task t = UpdateRowsWithParameters("MERCURY");
             //Task t = TestTransactions();
-            TestXMLReader();
-            TestXMLWriterCreateFile();
+            //TestXMLReader();
+            //TestXMLWriterCreateFile();
+            //TestXMLDoc();
+            TestXPath();
             Console.ReadKey();
         }
 
@@ -188,6 +191,80 @@ namespace _4_Implement_Data_Access
             }
 
             Console.WriteLine(stream.ToString());
+        }
+
+        public static void TestXMLDoc()
+        {
+            string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+                            <people>
+                                <person firstname=""john"" lastname=""doe"">
+                                    <contactdetails>
+                                        <emailaddress>john@unknown.com</emailaddress>
+                                    </contactdetails>
+                                </person>
+                                <person firstname=""jane"" lastname=""doe"">
+                                    <contactdetails>
+                                        <emailaddress>jane@unknown.com</emailaddress>
+                                        <phonenumber>001122334455</phonenumber>
+                                    </contactdetails>
+                                </person>
+                            </people>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            XmlNodeList nodes = doc.GetElementsByTagName("Person");
+
+            // Output the names of the people in the document.
+            foreach (XmlNode node in nodes)
+            {
+                string firstName = node.Attributes["firstName"].Value;
+                string lastName = node.Attributes["lastName"].Value;
+                Console.WriteLine("Name: {0} {1}", firstName, lastName);
+            }
+
+            // Start creating a new node.
+            XmlNode newNode = doc.CreateNode(XmlNodeType.Element, "person", "");
+            XmlAttribute firstNameAttribute = doc.CreateAttribute("firstName");
+            firstNameAttribute.Value = "Foo";
+            XmlAttribute lastNameAttribute = doc.CreateAttribute("lastName");
+            lastNameAttribute.Value = "Bar";
+            newNode.Attributes.Append(firstNameAttribute);
+            newNode.Attributes.Append(lastNameAttribute);
+            doc.DocumentElement.AppendChild(newNode);
+            Console.WriteLine("Modified xml...");
+            doc.Save(Console.Out);
+        }
+
+        public static void TestXPath()
+        {
+            string xml = @"<?xml version=""1.0"" encoding=""utf-8""?>
+                            <people>
+                                <person firstname=""john"" lastname=""doe"">
+                                    <contactdetails>
+                                        <emailaddress>john@unknown.com</emailaddress>
+                                    </contactdetails>
+                                </person>
+                                <person firstname=""jane"" lastname=""doe"">
+                                    <contactdetails>
+                                        <emailaddress>jane@unknown.com</emailaddress>
+                                        <phonenumber>001122334455</phonenumber>
+                                    </contactdetails>
+                                </person>
+                            </people>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            XPathNavigator nav = doc.CreateNavigator();
+            string query = "//people/person[@firstname='jane']";
+            XPathNodeIterator iterator = nav.Select(query);
+            Console.WriteLine(iterator.Count); // Displays 1
+
+            while (iterator.MoveNext())
+            {
+                string firstName = iterator.Current.GetAttribute("firstname","");
+                string lastName = iterator.Current.GetAttribute("lastname","");
+                Console.WriteLine("Name: {0} {1}", firstName, lastName);
+            }
         }
     }
 }
