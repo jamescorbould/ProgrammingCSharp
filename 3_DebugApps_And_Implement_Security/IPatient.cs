dda8bdd9b3f1677140c4bfc5157e793aba78fa4a
James Corbould <jamesc@datacom.co.nz>
2017-11-06 20:58:13 +1300

    Interface to test explicit interface implementation.

---
+++ 3_DebugApps_And_Implement_Security/IPatient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_DebugApps_And_Implement_Security
+{
+    interface IPatient
+    {
+        string GHNumber { get; set; }
+        string Name { get; set; }
+        string Age { get; set; }
+        System.Guid DoBulkLoad();
+    }
+}
